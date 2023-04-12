063eb6ce77f8ae2aed312b332521779833d448c3
rafaelcorreameta <rafael.gcorrea@meta.com.br>
2023-04-12 20:33:30 -0300

    Implements APIGateway

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -5,12 +5,43 @@ import * as cwlogs from "aws-cdk-lib/aws-logs";
 
 import { Construct } from "constructs";
 
+interface EcommerceApiStackProps extends cdk.StackProps {
+  productsFetchHandler: lambdaNodeJs.NodejsFunction;
+}
+
 export class EcommerceApiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: EcommerceApiStackProps) {
     super(scope, id, props);
+
+    const logGroup = new cwlogs.LogGroup(this, "EcommerceApiLogs");
 
     const api = new apigateway.RestApi(this, "ECommerceApi", {
       restApiName: "ECommerceApi",
+      cloudWatchRole: true,
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true,
+        }),
+      },
     });
+
+    const productsFetchIntegration = new apigateway.LambdaIntegration(
+      props.productsFetchHandler
+    );
+
+    // " no atributo root ja existe o (/) resultado => /products"
+    const productsResource = api.root.addResource("products");
+
+    //Criando chamada APIGateway
+    productsResource.addMethod("GET", productsFetchIntegration);
   }
 }
---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+import { ProductsAppStack } from "./productsApp-stack";
+import { EcommerceApiStack } from "./ecommerceApi-stack";
+
+export { ProductsAppStack, EcommerceApiStack };
