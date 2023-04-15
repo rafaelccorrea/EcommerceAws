7f73a17791a45451c1761b2e20f57e52e9208b7f
rafaelcorreameta <rafael.gcorrea@meta.com.br>
2023-04-15 13:56:11 -0300

    Implements connection Dynamodb and implements stack admin

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,14 +1,28 @@
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
 import * as cdk from "aws-cdk-lib";
+import * as dynadb from "aws-cdk-lib/aws-dynamodb";
 
 import { Construct } from "constructs";
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJs.NodejsFunction;
+  readonly productsAdminHandler: lambdaNodeJs.NodejsFunction;
+  readonly ProductsDdb = new dynadb.Table(this, "ProductsDdb", {
+    tableName: "products",
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+    partitionKey: {
+      name: "id",
+      type: dynadb.AttributeType.STRING,
+    },
+    billingMode: dynadb.BillingMode.PROVISIONED,
+    readCapacity: 1,
+    writeCapacity: 1,
+  });
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Stack Products
     this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(
       this,
       "ProductsFetchFunction",
@@ -22,7 +36,35 @@ export class ProductsAppStack extends cdk.Stack {
           minify: true,
           sourceMap: false,
         },
+        environment: {
+          PRODUCTS_DDB: this.ProductsDdb.tableName,
+        },
       }
     );
+    // Permission Read Table Stack Product
+    this.ProductsDdb.grantReadData(this.productsFetchHandler);
+
+    // Stack admin products
+    this.productsAdminHandler = new lambdaNodeJs.NodejsFunction(
+      this,
+      "ProductsAdminFunction",
+      {
+        functionName: "ProductsAdminFunction",
+        entry: "lambda/products/productsAdminFunction.ts",
+        handler: "handler",
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(5),
+        bundling: {
+          minify: true,
+          sourceMap: false,
+        },
+        environment: {
+          PRODUCTS_DDB: this.ProductsDdb.tableName,
+        },
+      }
+    );
+
+    // Permission Write Stack admin products
+    this.ProductsDdb.grantWriteData(this.productsAdminHandler);
   }
 }
